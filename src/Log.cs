8bc44d3a899ccc2d45f878427817238783a6702b
Yoni Lerner <yonilerner@gmail.com>
2024-01-03 17:52:26 -0500

    Multiproject with tests (#7)
    * Multiproject with tests
    
    * fix format
    
    * format

--- src/Log.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RetakesAllocator;
-
-public static class Log
-{
-    public static void Write(string message)
-    {
-        Console.WriteLine($"[{nameof(RetakesAllocator)}] {message}");
-    }
-}
