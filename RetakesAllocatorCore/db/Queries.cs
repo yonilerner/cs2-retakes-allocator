ea81280ea23b960002df4dd793be4b0f5e3f2ced
Yoni Lerner <yonilerner@gmail.com>
2024-01-06 17:08:20 -0500

    fix query

--- RetakesAllocatorCore/db/Queries.cs
+++ RetakesAllocatorCore/db/Queries.cs
@@ -13,14 +13,21 @@ public class Queries
 
     public static void SetWeaponPreferenceForUser(ulong userId, CsTeam team, RoundType roundType, CsItem? item)
     {
+        var isNew = false;
         var userSettings = Db.GetInstance().UserSettings.FirstOrDefault(u => u.UserId == userId);
         if (userSettings == null)
         {
             userSettings = new UserSetting {UserId = userId};
             Db.GetInstance().UserSettings.Add(userSettings);
+            isNew = true;
         }
 
         userSettings.SetWeaponPreference(team, roundType, item);
+        if (!isNew)
+        {
+            Db.GetInstance().Entry(userSettings).State = EntityState.Modified;
+        }
+
         Db.GetInstance().SaveChanges();
     }
 
@@ -28,7 +35,7 @@ public class Queries
     {
         var userSettingsList = Db.GetInstance()
             .UserSettings
-            // .AsNoTracking()
+            .AsNoTracking()
             .Where(u => userIds.Contains(u.UserId))
             .ToList();
         if (userSettingsList.Count == 0)
--- RetakesAllocatorTest/DbTests.cs
+++ RetakesAllocatorTest/DbTests.cs
@@ -36,24 +36,31 @@ public class DbTests
         Queries.SetWeaponPreferenceForUser(2, CsTeam.Terrorist, RoundType.FullBuy, CsItem.AK47);
 
         usersSettings = Queries.GetUsersSettings(new List<ulong> {1});
-        Assert.That(usersSettings.Keys, Is.EquivalentTo(new List<ulong> {1}));
-        Assert.That(usersSettings.Values.Select(v => v.UserId), Is.EquivalentTo(new List<ulong> {1}));
-
+        Assert.Multiple(() =>
+        {
+            Assert.That(usersSettings.Keys, Is.EquivalentTo(new List<ulong> { 1 }));
+            Assert.That(usersSettings.Values.Select(v => v.UserId), Is.EquivalentTo(new List<ulong> { 1 }));
+        });
         usersSettings = Queries.GetUsersSettings(new List<ulong> {2});
-        Assert.That(usersSettings.Keys, Is.EquivalentTo(new List<ulong> {2}));
-        Assert.That(usersSettings.Values.Select(v => v.UserId), Is.EquivalentTo(new List<ulong> {2}));
-
+        Assert.Multiple(() =>
+        {
+            Assert.That(usersSettings.Keys, Is.EquivalentTo(new List<ulong> { 2 }));
+            Assert.That(usersSettings.Values.Select(v => v.UserId), Is.EquivalentTo(new List<ulong> { 2 }));
+        });
         usersSettings = Queries.GetUsersSettings(new List<ulong> {1, 2});
-        Assert.That(usersSettings.Keys, Is.EquivalentTo(new List<ulong> {1, 2}));
-        Assert.That(usersSettings.Values.Select(v => v.UserId), Is.EquivalentTo(new List<ulong> {1, 2}));
+        Assert.Multiple(() =>
+        {
+            Assert.That(usersSettings.Keys, Is.EquivalentTo(new List<ulong> { 1, 2 }));
+            Assert.That(usersSettings.Values.Select(v => v.UserId), Is.EquivalentTo(new List<ulong> { 1, 2 }));
 
-        Assert.That(usersSettings[1].WeaponPreferences[CsTeam.Terrorist][RoundType.HalfBuy], Is.EqualTo(CsItem.MP5));
-        Assert.That(usersSettings[1].WeaponPreferences[CsTeam.Terrorist].TryGetValue(RoundType.Pistol, out _),
-            Is.EqualTo(false));
-        Assert.That(usersSettings[1].WeaponPreferences[CsTeam.CounterTerrorist][RoundType.FullBuy],
-            Is.EqualTo(CsItem.AWP));
-        Assert.That(usersSettings[1].WeaponPreferences[CsTeam.CounterTerrorist].TryGetValue(RoundType.HalfBuy, out _),
-            Is.EqualTo(false));
-        Assert.That(usersSettings[2].WeaponPreferences[CsTeam.Terrorist][RoundType.FullBuy], Is.EqualTo(CsItem.AK47));
+            Assert.That(usersSettings[1].WeaponPreferences[CsTeam.Terrorist][RoundType.HalfBuy], Is.EqualTo(CsItem.MP5));
+            Assert.That(usersSettings[1].WeaponPreferences[CsTeam.Terrorist].TryGetValue(RoundType.Pistol, out _),
+                Is.EqualTo(false));
+            Assert.That(usersSettings[1].WeaponPreferences[CsTeam.CounterTerrorist][RoundType.FullBuy],
+                Is.EqualTo(CsItem.AWP));
+            Assert.That(usersSettings[1].WeaponPreferences[CsTeam.CounterTerrorist].TryGetValue(RoundType.HalfBuy, out _),
+                Is.EqualTo(false));
+            Assert.That(usersSettings[2].WeaponPreferences[CsTeam.Terrorist][RoundType.FullBuy], Is.EqualTo(CsItem.AK47));
+        });
     }
 }
