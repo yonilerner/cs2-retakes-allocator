97a02c5cd48ca7085fb4a8920d5ec714bf75046f
Yoni Lerner <yonilerner@gmail.com>
2024-01-10 21:18:10 -0500

    Build (#16)
    * build
    
    * release
    
    * newline
    
    * powershell
    
    * windows
    
    * shells cript
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a

---
+++ RetakesAllocator/release.bash
@@ -0,0 +1,22 @@
+﻿#!/usr/bin/env bash
+
+TARGET_NAME="RetakesAllocator"
+TARGET_DIR="./bin/Release/net7.0"
+NEW_DIR="$TARGET_DIR/../$TARGET_NAME"
+
+echo $TARGET_NAME
+echo $TARGET_DIR
+echo $NEW_DIR
+
+ls $TARGET_DIR/**
+
+echo mkdir -p $NEW_DIR
+mkdir -p $NEW_DIR
+echo cp -rf "$TARGET_DIR/runtimes/linux-x64" "$NEW_DIR/runtimes"
+cp -rf "$TARGET_DIR/runtimes/linux-x64" "$NEW_DIR/runtimes"
+echo cp -rf "$TARGET_DIR/runtimes/win-x64" "$NEW_DIR/runtimes"
+cp -rf "$TARGET_DIR/runtimes/win-x64" "$NEW_DIR/runtimes"
+echo zip -r "$TARGET_NAME.zip" "$NEW_DIR/"
+zip -r "./bin/Release/$TARGET_NAME.zip" "$NEW_DIR/"
+
+ls ./bin/**
