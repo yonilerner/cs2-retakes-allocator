389a546d7c1c6183377ff890e5a250c1b0dfcd2c
Yoni Lerner <yonilerner@gmail.com>
2024-01-10 20:46:42 -0500

    a

--- RetakesAllocator/release.sh
+++ RetakesAllocator/release.sh
@@ -1,11 +1,4 @@
-﻿rm -Recurse -Force &quot;$(TargetDir)..\$(TargetName)&quot;
-mkdir -p &quot;$(TargetDir)..\$(TargetName)&quot;
-cp -Force &quot;$(TargetDir)* $(TargetDir)..\$(TargetName)&quot;
-cp -Force -Recurse &quot;$(TargetDir)runtimes\linux-x64 $(TargetDir)..\$(TargetName)\runtimes&quot;
-cp -Force -Recurse &quot;$(TargetDir)runtimes\win-x64 $(TargetDir)..\$(TargetName)\runtimes&quot;
-Compress-Archive -Force -Path &quot;$(TargetDir)..\$(TargetName)\*&quot; -DestinationPath &quot;$(TargetDir)..\$(TargetName).zip&quot; 
-
-TARGET_NAME="RetakesAllocator"
+﻿TARGET_NAME="RetakesAllocator"
 TARGET_DIR="./bin/Release/net7.0"
 NEW_DIR="$TARGET_DIR/../$TARGET_NAME"
 
