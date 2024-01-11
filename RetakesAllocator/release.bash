a4e7ba6563ef53cb288ffbd46297e0dd68c3afc5
Yoni Lerner <yonilerner@gmail.com>
2024-01-10 21:35:21 -0500

    a

--- RetakesAllocator/release.bash
+++ RetakesAllocator/release.bash
@@ -16,7 +16,7 @@ echo cp -rf "$TARGET_DIR/runtimes/linux-x64" "$NEW_DIR/runtimes"
 cp -rf "$TARGET_DIR/runtimes/linux-x64" "$NEW_DIR/runtimes"
 echo cp -rf "$TARGET_DIR/runtimes/win-x64" "$NEW_DIR/runtimes"
 cp -rf "$TARGET_DIR/runtimes/win-x64" "$NEW_DIR/runtimes"
-#echo zip -r "$TARGET_NAME.zip" "$NEW_DIR/"
-#zip -r "./bin/Release/$TARGET_NAME.zip" "$NEW_DIR/"
-#
-#ls ./bin/**
+echo zip -r "$TARGET_NAME.zip" "$NEW_DIR/"
+zip -r "./bin/Release/$TARGET_NAME.zip" "$NEW_DIR/"
+
+ls ./bin/**
