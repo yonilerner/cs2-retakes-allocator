8d9df6f75dc5ac6b3e1144aca72d20902bc8c4c6
Yoni Lerner <yonilerner@gmail.com>
2024-01-10 21:47:43 -0500

    test (#17)
    * test
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a

--- RetakesAllocator/release.bash
+++ RetakesAllocator/release.bash
@@ -2,7 +2,7 @@
 
 TARGET_NAME="RetakesAllocator"
 TARGET_DIR="./bin/Release/net7.0"
-NEW_DIR="$TARGET_DIR/../$TARGET_NAME"
+NEW_DIR="./bin/Release/RetakesAllocator"
 
 echo $TARGET_NAME
 echo $TARGET_DIR
@@ -10,13 +10,19 @@ echo $NEW_DIR
 
 ls $TARGET_DIR/**
 
-echo mkdir -p $NEW_DIR
-mkdir -p $NEW_DIR
+#echo mkdir -p $NEW_DIR
+#mkdir -p $NEW_DIR
+echo cp -r $TARGET_DIR $NEW_DIR
+cp -r $TARGET_DIR $NEW_DIR
+echo rm -rf "$NEW_DIR/runtimes"
+rm -rf "$NEW_DIR/runtimes"
+echo mkdir "$NEW_DIR/runtimes"
+mkdir "$NEW_DIR/runtimes"
 echo cp -rf "$TARGET_DIR/runtimes/linux-x64" "$NEW_DIR/runtimes"
 cp -rf "$TARGET_DIR/runtimes/linux-x64" "$NEW_DIR/runtimes"
 echo cp -rf "$TARGET_DIR/runtimes/win-x64" "$NEW_DIR/runtimes"
 cp -rf "$TARGET_DIR/runtimes/win-x64" "$NEW_DIR/runtimes"
-echo zip -r "$TARGET_NAME.zip" "$NEW_DIR/"
-zip -r "./bin/Release/$TARGET_NAME.zip" "$NEW_DIR/"
+#echo zip -r "$TARGET_NAME.zip" "$NEW_DIR/"
+#zip -r "./bin/Release/$TARGET_NAME.zip" "$NEW_DIR/"
 
-ls ./bin/**
+tree ./bin
