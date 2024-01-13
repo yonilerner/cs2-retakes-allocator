3722d37a246dd8e2e8f1c66f16c190a23df1693f
B3none <ablackham2000@gmail.com>
2024-01-13 22:01:41 +0000

    Started implementing VoteManager

--- RetakesAllocator/Managers/VoteManager.cs
+++ RetakesAllocator/Managers/VoteManager.cs
@@ -1,8 +1,25 @@
-using RetakesAllocatorCore;
+using CounterStrikeSharp.API.Core;
 
 namespace RetakesAllocator.Managers;
 
 public class VoteManager
 {
+    protected const float VoteTimeout = 30.0f;
     
+    private bool _isVoteInProgress;
+    private readonly HashSet<CCSPlayerController> _votes = new();
+    
+    public void CastVote(CCSPlayerController player)
+    {
+        if (_isVoteInProgress)
+        {
+            player.PrintToChat($"{MessagePrefix}A vote is already in progress!");
+            return;
+        }
+        
+        _isVoteInProgress = true;
+        _votes.Add(player);
+        
+        player.PrintToChat($"{MessagePrefix}A vote has been started! Type /vote to vote!");
+    }
 }
