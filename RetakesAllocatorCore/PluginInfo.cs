ae6855f1f897f02e3a7b462eccef40417fecdae7
Yoni Lerner <yonilerner@gmail.com>
2024-01-14 19:08:23 -0500

    version

--- RetakesAllocatorCore/PluginInfo.cs
+++ RetakesAllocatorCore/PluginInfo.cs
@@ -4,7 +4,7 @@ namespace RetakesAllocatorCore;
 
 public static class PluginInfo
 {
-    public const string Version = "1.1.1";
+    public const string Version = "1.2.0";
     
     public static readonly string LogPrefix = $"[RetakesAllocator {Version}] ";
     public static readonly string MessagePrefix = $"[{ChatColors.Green}Retakes{ChatColors.White}] ";
