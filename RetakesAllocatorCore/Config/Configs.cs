b033b281a2c44146ea5a686f1a30d31c0b9c300e
JesseMH <jessemh321@gmail.com>
2024-01-21 17:50:29 -0500

    config change

--- RetakesAllocatorCore/Config/Configs.cs
+++ RetakesAllocatorCore/Config/Configs.cs
@@ -14,13 +14,11 @@ public static class Configs
 
     private static readonly JsonSerializerOptions SerializationOptions = new()
     {
-        Converters =
-        {
-            new JsonStringEnumConverter()
-        },
+        Converters = { new JsonStringEnumConverter() },
         WriteIndented = true,
         AllowTrailingCommas = true,
         ReadCommentHandling = JsonCommentHandling.Skip,
+        DefaultIgnoreCondition = JsonIgnoreCondition.Never
     };
 
     public static bool IsLoaded()
