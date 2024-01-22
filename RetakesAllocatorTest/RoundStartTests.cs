7697b4e8ad05a65c0e155659962da6ce3211fef1
Yoni Lerner <yonilerner@gmail.com>
2024-01-21 20:17:13 -0500

    Tests ensure core separation

---
+++ RetakesAllocatorTest/RoundStartTests.cs
@@ -0,0 +1,21 @@
+﻿using CounterStrikeSharp.API.Modules.Utils;
+using RetakesAllocatorCore;
+
+namespace RetakesAllocatorTest;
+
+public class RoundStartTests : BaseTestFixture
+{
+    [Test]
+    public void TestRoundStartCanRunInCore()
+    {
+        OnRoundPostStartHelper.Handle(
+            new List<int>(),
+            i => 1,
+            x => CsTeam.None,
+            x => {},
+            (x, y, z) => {},
+            x => false,
+            out _
+        );
+    }
+}
