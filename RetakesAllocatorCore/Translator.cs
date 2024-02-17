dbe47b60abfacbc27be10361277bdd942a1a15ea
Yoni Lerner <yonilerner@gmail.com>
2024-02-16 22:40:36 -0800

    remove comments

--- RetakesAllocatorCore/Translator.cs
+++ RetakesAllocatorCore/Translator.cs
@@ -49,31 +49,7 @@ public class Translator
         }
 
         var translation = localizedString.Value;
-        
-        // Handle translation colours
-        // return translation
-        //     .Replace("[GREEN]", isHtml ? "<font color='green'>" : ChatColors.Green.ToString())
-        //     .Replace("[RED]", isHtml ? "<font color='red'>" : ChatColors.Red.ToString())
-        //     .Replace("[YELLOW]", isHtml ? "<font color='yellow'>" : ChatColors.Yellow.ToString())
-        //     .Replace("[BLUE]", isHtml ? "<font color='blue'>" : ChatColors.Blue.ToString())
-        //     .Replace("[PURPLE]", isHtml ? "<font color='purple'>" : ChatColors.Purple.ToString())
-        //     .Replace("[ORANGE]", isHtml ? "<font color='orange'>" : ChatColors.Orange.ToString())
-        //     .Replace("[WHITE]", isHtml ? "<font color='white'>" : ChatColors.White.ToString())
-        //     .Replace("[NORMAL]", isHtml ? "<font color='white'>" : ChatColors.White.ToString())
-        //     .Replace("[GREY]", isHtml ? "<font color='grey'>" : ChatColors.Grey.ToString())
-        //     .Replace("[LIGHT_RED]", isHtml ? "<font color='lightred'>" : ChatColors.LightRed.ToString())
-        //     .Replace("[LIGHT_BLUE]", isHtml ? "<font color='lightblue'>" : ChatColors.LightBlue.ToString())
-        //     .Replace("[LIGHT_PURPLE]", isHtml ? "<font color='mediumpurple'>" : ChatColors.LightPurple.ToString())
-        //     .Replace("[LIGHT_YELLOW]", isHtml ? "<font color='lightyellow'>" : ChatColors.LightYellow.ToString())
-        //     .Replace("[DARK_RED]", isHtml ? "<font color='darkred'>" : ChatColors.DarkRed.ToString())
-        //     .Replace("[DARK_BLUE]", isHtml ? "<font color='darkblue'>" : ChatColors.DarkBlue.ToString())
-        //     .Replace("[BLUE_GREY]", isHtml ? "<font color='grey'>" : ChatColors.BlueGrey.ToString())
-        //     .Replace("[OLIVE]", isHtml ? "<font color='olive'>" : ChatColors.Olive.ToString())
-        //     .Replace("[LIME]", isHtml ? "<font color='lime'>" : ChatColors.Lime.ToString())
-        //     .Replace("[GOLD]", isHtml ? "<font color='gold'>" : ChatColors.Gold.ToString())
-        //     .Replace("[SILVER]", isHtml ? "<font color='silver'>" : ChatColors.Silver.ToString())
-        //     .Replace("[MAGENTA]", isHtml ? "<font color='magenta'>" : ChatColors.Magenta.ToString());
-        
+
         return translation
             .Replace("[GREEN]", isCenter ? "" : ChatColors.Green.ToString())
             .Replace("[RED]", isCenter ? "" : ChatColors.Red.ToString())
