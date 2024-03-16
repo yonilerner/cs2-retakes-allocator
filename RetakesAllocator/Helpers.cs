e5cc87002cc61c5810ff1733e7a0c4328d6949bb
Gold KingZ <48490385+oqyh@users.noreply.github.com>
2024-03-16 04:35:53 +0400

    Update Helpers.cs

--- RetakesAllocator/Helpers.cs
+++ RetakesAllocator/Helpers.cs
@@ -146,7 +146,7 @@ public static class Helpers
 
             // Log.Write($"Removing weapon {weapon.Value.DesignerName} {weapon.IsValid}");
 
-            player.PlayerPawn.Value.RemovePlayerItem(weapon.Value);
+            Utilities.RemoveItemByDesignerName(player, weapon.Value.ToString()!, true);
             weapon.Value.Remove();
 
             removed = true;
