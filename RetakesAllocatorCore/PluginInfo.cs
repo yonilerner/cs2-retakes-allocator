25bb608da65a8f512ec760db3a54088e56548d4d
Yoni Lerner <yoni.lerner@discordapp.com>
2024-04-26 13:19:41 -0400

    2.3.6

--- RetakesAllocatorCore/PluginInfo.cs
+++ RetakesAllocatorCore/PluginInfo.cs
@@ -5,7 +5,7 @@ namespace RetakesAllocatorCore;
 
 public static class PluginInfo
 {
-    public const string Version = "2.3.5";
+    public const string Version = "2.3.6";
 
     public static readonly string LogPrefix = $"[RetakesAllocator {Version}] ";
 
