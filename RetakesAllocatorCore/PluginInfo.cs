3b13da5c4ef3ddf443389a904b5a6058c7562489
Yoni Lerner <yonilerner@gmail.com>
2024-12-05 14:03:32 -0800

    Update PluginInfo.cs

--- RetakesAllocatorCore/PluginInfo.cs
+++ RetakesAllocatorCore/PluginInfo.cs
@@ -5,7 +5,7 @@ namespace RetakesAllocatorCore;
 
 public static class PluginInfo
 {
-    public const string Version = "2.3.16";
+    public const string Version = "2.3.17";
 
     public static readonly string LogPrefix = $"[RetakesAllocator {Version}] ";
 
